5416ac43167b1a25226278f89ebabcd7fca01f86
Quốc Việt <thaibinhquocviet2002@gmail.com>
2024-08-23 22:15:40 +0700

    install novi chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return <div>Hello World</div>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
